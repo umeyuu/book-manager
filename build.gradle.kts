9798d84398229e16b272196d9d53c4f6bdb404a3
umeyuu <91179464+umeyuu@users.noreply.github.com>
2024-06-15 16:42:48 +0900

    初期設定

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
+	id("com.thinkimi.gradle.MybatisGenerator") version "2.4"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
 }
@@ -27,6 +28,10 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1") // 追加
+	implementation("mysql:mysql-connector-java:8.0.23") // 追加
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0") // 追加
 }
 
 kotlin {
@@ -37,4 +42,10 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+// 追加
+mybatisGenerator {
+	verbose = true
+	configFile = "/Users/umedayuusuke/IdeaProjects/book-manager/src/main/resources/generatorConfig.xml"
 }
